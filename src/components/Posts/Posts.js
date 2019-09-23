b322e7e2cb2240afdbf9c99b04b1a4d3b2845abd
AB3000 <aarushban@gmail.com>
2019-09-23 16:25:42 -0400

    Renamed Posts file to posts components

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './Posts.css';
 
 function Posts() {
   return (
--- src/components/Posts/Posts.test.js
+++ src/components/Posts/Posts.test.js
@@ -4,6 +4,6 @@ import App from './Posts';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Posts />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
