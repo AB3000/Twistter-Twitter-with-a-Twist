d1889b4d2523b34d0f46ac6af2489dc193dace5f
AB3000 <aarushban@gmail.com>
2019-09-23 18:03:31 -0400

    Added express server

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,28 +1,45 @@
-var mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
 
-//Set up default mongoose connection
-var mongoDB = 'mongodb://localhost:3000/Twistter';
-mongoose.connect(mongoDB, { useNewUrlParser: true });
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
-//Get the default connection
-var db = mongoose.connection;
-
-//Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-var Schema = mongoose.Schema;
-
-var SomeModelSchema = new Schema({
-    a_string: String,
-    a_date: Date
-  });
-
-  var Schema = mongoose.Schema;
-
-var SomeModelSchema = new Schema({
-  a_string: String,
-  a_date: Date
+// create a GET route
+app.get('/express_backend', (req, res) => {
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
 
-// Compile model from schema
-var SomeModel = mongoose.model('SomeModel', SomeModelSchema );
+var mongoose = require('mongoose');
+console.log('HERE WOMEN');
+
+
+
+//Set up default mongoose connection
+// var mongoDB = 'mongodb://localhost:3000/Twistter';
+// mongoose.connect(mongoDB, { useNewUrlParser: true });
+
+// //Get the default connection
+// var db = mongoose.connection;
+
+// //Bind connection to error event (to get notification of connection errors)
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// var Schema = mongoose.Schema;
+
+// var SomeModelSchema = new Schema({
+//     a_string: String,
+//     a_date: Date
+//   });
+
+//   var Schema = mongoose.Schema;
+
+// var SomeModelSchema = new Schema({
+//   a_string: String,
+//   a_date: Date
+// });
+
+// console.log('SEX FUCK YEAH!'); 
+
+// // Compile model from schema
+// var SomeModel = mongoose.model('SomeModel', SomeModelSchema );
--- src/frontend/components/Home/Home.js
+++ src/frontend/components/Home/Home.js
@@ -1,20 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Home.css';
 
-function Home() {
+class Home extends Component{
+
+  state = {
+    data: null
+  };
+
+  componentDidMount() {
+    // Call our fetch function below once the component mounts
+  this.callBackendAPI()
+    .then(res => this.setState(
+      { data: res.express }
+      ))
+    .catch(err => console.log(err));
+}
+  // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+callBackendAPI = async () => {
+  const response = await fetch('/express_backend');
+  const body = await response.json();
+
+
+  if (response.status !== 200) {
+    throw Error(body.message) 
+  }
+
+  console.log("BODY IS", body);
+  return body;
+};
+
+render () {
   return (
     <div id="absoluteCenteredDiv">
         <form>
-            <div class="box">
+            <div className="box">
                 <h1>Login Form</h1>
-                <input class="username" name="username" type="text" placeholder="User Name"></input>
-                <input class="username" name="username" type="password" placeholder="Password"></input>
-                <a href="#"><div class="button">Sign In</div></a>
+                <input className="username" name="username" type="text" placeholder="User Name"></input>
+                <input className="username" name="username" type="password" placeholder="Password"></input>
+                <a href="#"><div className="button">Sign In</div></a>
             </div>
         </form>
-        <p>Forgot your password? <a class="fpwd" href="#">Click Here!</a></p>
+        <p>Forgot your password? <a className="fpwd" href="#">Click Here!</a></p>
     </div> 
   );
+}
+
 }
 
 export default Home;
