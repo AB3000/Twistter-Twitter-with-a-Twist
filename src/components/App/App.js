499bd64f4ffe9c7749a7efc0a059c9b8b16b1cf1
AB3000 <aarushban@gmail.com>
2019-09-23 16:23:26 -0400

    App.js now contains top-level routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import './App.css';
-
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import Posts from './components/Posts';
 function App() {
   return (
-    <div id="absoluteCenteredDiv">
-        <form action="index.html" method="post">
-            <div class="box">
-                <h1>Login Form</h1>
-                <input class="username" name="username" type="text" placeholder="User Name"></input>
-                <input class="username" name="username" type="password" placeholder="Password"></input>
-                <a href="#"><div class="button">Sign In</div></a>
-            </div>
-        </form>
-        <p>Forgot your password? <a class="fpwd" href="#">Click Here!</a></p>
-    </div> 
+    <Router>
+        <div>
+          {/* Top-Level Routes */}
+          <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/Posts' component={Posts} />
+          </Switch>
+        </div>
+      </Router>
   );
 }
 
