aaa44424d90888b3f454e3d11b566d198f88b38f
AB3000 <aarushban@gmail.com>
2019-09-23 16:08:49 -0400

    Reformatted css and app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,11 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-  
-      </header>
-    </div>
+    <div id="absoluteCenteredDiv">
+        <form action="index.html" method="post">
+            <div class="box">
+                <h1>Login Form</h1>
+                <input class="username" name="username" type="text" placeholder="User Name"></input>
+                <input class="username" name="username" type="password" placeholder="Password"></input>
+                <a href="#"><div class="button">Sign In</div></a>
+            </div>
+        </form>
+        <p>Forgot your password? <a class="fpwd" href="#">Click Here!</a></p>
+    </div> 
   );
 }
 
