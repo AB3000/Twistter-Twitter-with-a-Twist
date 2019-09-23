98735b869882cfd9af34b3d7348094efe3836cd7
AB3000 <aarushban@gmail.com>
2019-09-23 16:24:31 -0400

    Renamed posts files

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+function Posts() {
   return (
-    <div className="App">
+    <div className="Posts">
       <header className="App-header">
   
       </header>
