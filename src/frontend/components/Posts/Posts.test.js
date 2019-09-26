8d15496732b4e06869d9611bfa168d935b574cb7
ShwethaUmashanker <shwthshanker@gmail.com>
2019-09-25 22:32:05 -0400

    new node app

--- src/frontend/components/Posts/Posts.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './Posts';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Posts />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
