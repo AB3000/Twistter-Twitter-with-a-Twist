419742a4bc40514d5ede2922bc6eef462f205433
andyw24 <andyw24@gmail.com>
2019-09-28 17:24:11 -0400

    added sha1 encryption to password

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || "5000";
 
+var AES = require("crypto-js/aes");
+var SHA256 = require("crypto-js/sha256");
+var CryptoJS = require("crypto-js");
+console.log(CryptoJS.HmacSHA1("Message", "Key"));
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'login.html'));
 });
 
-app.get('/login', function (req, res,html) {
+app.get('/signup', function (req, res,html) {
  res.sendFile(path.join(__dirname+'/signup.html'));
 });
 
@@ -29,17 +34,23 @@ console.log("Couldn't connect to database");
 
 //Login, Logout, Signup
 app.use(express.urlencoded())
-app.post("/signup", (req, res) => {
+app.post("/signupAfter", (req, res) => {
   //receiving form information from signup.html 
   const e = req.body.email;
+  const u = req.body.username;
   const p = req.body.password;
-  res.status(200).send(e);
+  
+  //using CryptoJS to encrypt password
+  encrypttedP = CryptoJS.SHA1(p);
+  encrypttedP = encrypttedP.toString(CryptoJS.enc.Base64);
+  res.status(200).send(encrypttedP);
   res.end();
 
   //formatting the email and password info into the user schema
   var newUser = new user({
-  	email: e, 
-  	password: p
+  	email: e,
+  	username: u,  
+  	password: encrypttedP
   });
 
   //saving the new user to the database
