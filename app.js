c067c7975dba7f1c92d1a5dadd984aadf54207c5
ShwethaUmashanker <shwthshanker@gmail.com>
2019-09-28 15:34:39 -0400

    connecting to mongo

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const http = require('http');
 const path = require("path");
 const express = require("express");
 
+
 const app = express();
 const port = process.env.PORT || "5000";
 
@@ -11,4 +12,18 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Listening to requests on http://localhost:${port}`);
+});
+
+
+
+var mongoose = require("mongoose");
+var passport = require("passport");
+var bodyParser = require("body-parser");  
+
+mongoose.connect('mongodb+srv://Twistter:CS30700!@twistter-dcrea.mongodb.net/Twistter307?retryWrites=true&w=majority',{useNewUrlParser: true},function(error){
+  if(error){
+console.log("Couldn't connect to database");
+    } else {
+      console.log("Connected To Database");
+    }
 });
