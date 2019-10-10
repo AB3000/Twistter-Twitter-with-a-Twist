a1e4cdf352a32e40e542061febc7f032e4857b97
andyw24 <andyw24@gmail.com>
2019-10-10 18:20:37 -0400

    updated postschema

--- models/post.js
+++ models/post.js
@@ -5,6 +5,10 @@ let postSchema = new Schema({
     title: {type: String, required: false},
     topic: {type: String, required: false},
 	description: {type: String, required: false},
+	date: {type: date, required: true}, 
+	user: {type: String, required: true},  
+	likes: {type: int, required: true}, 
+	dislikes: {type: int, required: true}
 });
 // Export the model
 module.exports = mongoose.model('post', postSchema);
