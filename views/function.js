204a6fc7e7125294438e8b81986adf8282836874
AB3000 <aarushban@gmail.com>
2019-11-21 19:25:38 -0500

    Removed external function page

--- views/function.js
+++
@@ -1,9 +0,0 @@
-
-// console.log("it's included!");
-// exports = module.exports = {};
-
-// exports.expand = function() {
-//     console.log("HERE YOU PIECES OF STUFF")
-//     // document.getElementById('textInput').className="show";
-//     $('#textInput').className="show";
-// }
